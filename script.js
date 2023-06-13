321e7cb8aff612a44798a3ed57a3375285068989
Arslan <marslanmustafa391@gmail.com>
2023-06-13 21:16:30 +0500

    search bar activated

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-// let search = ()=>{
-// let input = document.getElementById("input");
-//   let val = input.value;
-//   let keyWord = val.replaceAll(" ","+");
-//   console.log(keyWord);
-// let web = "https://www.google.com/search?q="+keyWord+"&rlz=1C1SQJL_enPK1000PK1000"
-// }
+let search = ()=>{
+let input = document.querySelector("input");
+  let val = input.value;
+  let keyWord = val.replaceAll(" ","+");
+  console.log(keyWord);
+let web = "https://www.google.com/search?q="+keyWord+"&rlz=1C1SQJL_enPK1000PK1000"
+console.log(web);
+location.href = web;
+}
 
 
-let input = document.querySelector(".input") 
-let valu = input.value  
-let k = valu.replaceAll(" ","+")  
-console.log(k)
-// input.addEventListener("keypress", (event)=> {
-//   if (event.key === "Enter") {
-//     event.preventDefault();
-//     document.querySelector(".right-icon").click();
+// let input = document.querySelector(".input") 
+// let valu = input.value  
+// let k = valu.replaceAll(" ","+")  
+// console.log(k)
+let input = document.querySelector("input");
+input.addEventListener("keypress", (event)=> {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.querySelector(".right-icon").click();
     
-//   }
-// });
+  }
+});
